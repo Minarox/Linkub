0a598cd7f66ff6aff344f2644f15f0da0dd3cbca
Mathis S.M. <contact@minarox.fr>
2025-02-11 09:58:50 +0100

    build: Add astro-compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import compressor from 'astro-compressor';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://minarox.github.com",
     base: "/linkub",
-    integrations: [sitemap()]
+    integrations: [sitemap(), compressor({ gzip: false })]
 });

d6007a86c4962ac768ce4ca8b80268a45861bf19
Minarox <contact@minarox.fr>
2025-02-11 10:17:37 +0100

    build: Add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import metaTags from 'astro-meta-tags';
+import icon from 'astro-icon';
 import compressor from 'astro-compressor';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://minarox.github.com",
     base: "/linkub",
-    integrations: [sitemap(), metaTags(), compressor({ gzip: false })]
+    integrations: [sitemap(), metaTags(), icon(), compressor({ gzip: false })]
 });

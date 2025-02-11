295a2fd1dc260a73939860310d8d89ee5ed7e1c2
Minarox <contact@minarox.fr>
2025-02-11 10:11:23 +0100

    build: Add astro-meta-tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import metaTags from 'astro-meta-tags';
 import compressor from 'astro-compressor';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://minarox.github.com",
     base: "/linkub",
-    integrations: [sitemap(), compressor({ gzip: false })]
+    integrations: [sitemap(), metaTags(), compressor({ gzip: false })]
 });

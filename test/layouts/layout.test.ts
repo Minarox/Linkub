2c1c2c1ca7e739200b0f2319da181c7cfc83bd9a
Mathis S.M. <m.sema@exatech-group.com>
2025-03-10 11:09:23 +0000

    refactor: Move all config in index file

--- test/layouts/layout.test.ts
+++ test/layouts/layout.test.ts
@@ -1,25 +1,39 @@
 import { test, expect } from 'vitest';
 import { experimental_AstroContainer as AstroContainer } from 'astro/container';
-import Layout from '@layouts/Layout.astro';
+import Layout, { type Props as LayoutProps } from '@layouts/Layout.astro';
+
+export type Props = {
+    title: string;
+    description: string;
+    keywords: string;
+    creator: string;
+    [key: string]: any;
+};
 
 test('Main layout', async () => {
 	const container: AstroContainer = await AstroContainer.create();
-	const component: Response = await container.renderToResponse(Layout);
-	const title: string = 'Linkub'
-	const description: string = "All links in one place."
+	const props: LayoutProps = {
+		title: 'Linkub',
+		description: 'All links in one place.',
+		keywords: 'Linkhub, Link, social media, hub, landing page, web',
+		creator: 'Minarox'
+	};
+	const component: Response = await container.renderToResponse(Layout, {
+		props: { ...props }
+	});
 
 	expect(component.status).toBe(200);
 	expect(component.headers.get('Content-Type')).toBe('text/html');
 
 	const body: string = await component.text();
-	expect(body).toContain(`<title>${title}</title>`);
-	expect(body).toContain(`<meta name="description" content="${description}">`);
-	expect(body).toContain(`<meta property="og:title" content="${title}">`);
+	expect(body).toContain(`<title>${props.title}</title>`);
+	expect(body).toContain(`<meta name="description" content="${props.description}">`);
+	expect(body).toContain(`<meta property="og:title" content="${props.title}">`);
 	// expect(body).toContain('<meta property="og:image" content="/banner.png">');
-	expect(body).toContain(`<meta property="og:image:alt" content="${title} banner">`);
-	expect(body).toContain(`<meta name="twitter:title" content="${title}">`);
-	expect(body).toContain(`<meta name="twitter:description" content="${description}">`);
+	expect(body).toContain(`<meta property="og:image:alt" content="${props.title} banner">`);
+	expect(body).toContain(`<meta name="twitter:title" content="${props.title}">`);
+	expect(body).toContain(`<meta name="twitter:description" content="${props.description}">`);
 	// expect(body).toContain('<meta name="twitter:image" content="/banner.png">');
-	expect(body).toContain(`<meta name="twitter:image:alt" content="${title} banner">`);
+	expect(body).toContain(`<meta name="twitter:image:alt" content="${props.title} banner">`);
 	expect(body).toContain('<link rel="sitemap" href="/sitemap-index.xml">');
 })
